1d69cd27d45130c872d9a4146cfbe0791f1607a3
sovendea77 <115879243+sovendea77@users.noreply.github.com>
2023-04-14 20:22:03 +0800

    上传视频，读取选项

---
+++ Assets/Mune/Scripts/CheckLoad.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class CheckLoad : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public GameObject checkCanvas;
+    public GameObject vedioCanvas;
+    public bool ischeck;
+    
+    public void Check()
+    {
+        ischeck = true;
+    }
+    public void NotLoad()
+    {
+        checkCanvas.SetActive(false);
+        vedioCanvas.SetActive(true);
+        videoPlayer.Play();
+        Invoke("Check", 2f);
+    }
+
+    void Start()
+    {
+        ischeck = false;
+        videoPlayer.targetTexture = new RenderTexture((int)rawImage.rectTransform.rect.width, (int)rawImage.rectTransform.rect.height, 0);
+        rawImage.texture = videoPlayer.targetTexture;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(videoPlayer.isPlaying);
+        if(!videoPlayer.isPlaying && ischeck)
+        {
+            SceneManager.LoadScene("Saint");
+        }
+    }
+}
---
+++ Assets/Mune/Scripts/MuneButtons.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Drawing;
+using UnityEngine.UIElements;
+
+public class MuneButtons : MonoBehaviour
+{
+    public GameObject black;
+    public GameObject startB;
+    public GameObject quitB;
+    public bool isstart;
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        isstart = true;
+        startB.SetActive(false);
+        quitB.SetActive(false);
+
+    }
+
+    private void Start()
+    {
+        isstart = false;
+
+    }
+    void Update()
+    {
+        if(isstart)
+        {
+            float color = black.GetComponent<SpriteRenderer>().color.a;
+            float a = Mathf.Lerp(color, 1, 0.5f * Time.deltaTime);
+            Debug.Log(color);
+            black.GetComponent<SpriteRenderer>().color = new UnityEngine.Color(0, 0, 0, a);
+        }
+
+        if(black.GetComponent<SpriteRenderer>().color.a > 0.95f && isstart)
+        {
+            SceneManager.LoadScene("Xuzhang");
+        }
+    }
+}
